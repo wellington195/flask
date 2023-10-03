0cb8ed79924d0185d30a83fda65a591bf7a3a7fc
wellington195 <106635796+wellington195@users.noreply.github.com>
2023-10-03 10:27:26 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,4 +1,43 @@
 
-from api import create_app
+import os
+from flask import Flask
 
-create_app()
+
+#flask --app api run --debug
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    # Configuration
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Register Blueprints
+    from . import  auth, db, blog
+   
+    app.register_blueprint(blog.bp)
+    app.register_blueprint(auth.bp)
+
+    # Add URL Rules
+    app.add_url_rule('/', endpoint='index')
+
+    # Initialize the database
+    db.init_app(app)
+    return app.run
+   
+
